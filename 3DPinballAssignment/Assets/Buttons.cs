fbd2a1c4fc9d86c0a85b3e4622eea75dcb519ffa
VoidNinjaL22 <55792700+VoidNinjaL22@users.noreply.github.com>
2020-06-07 16:23:08 +0100

    Pinball

---
+++ 3DPinballAssignment/Assets/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public bool PressA = false;
+    public bool PressD = false;
+
+    public string inputName;
+    public string outputName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("LFlip"))
+        {
+
+        }
+    }
+}
